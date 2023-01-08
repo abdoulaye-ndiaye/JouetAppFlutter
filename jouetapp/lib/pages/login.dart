f7fdb31048cce1f835774465d4521db4672271b2
abdoulaye-ndiaye <wizlaye7@gmail.com>
2023-01-08 10:03:30 +0000

    login

--- jouetapp/lib/pages/login.dart
+++ jouetapp/lib/pages/login.dart
@@ -16,71 +16,67 @@ class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/produits/téléchargement.jpeg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Mon application', style: TextStyle(fontSize: 40.0, color: Colors.white)),
-                const Text('De Jeux pour Enfant', style: TextStyle(fontSize: 20.0, color: Colors.white)),
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'Adresse email',
-                          labelStyle: TextStyle(color: Colors.white),
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Veuillez entrer une adresse email valide';
-                          }
-                          return null;
-                        },
-                        onSaved: (value) => _email = value!,
+      appBar: AppBar(
+        title: const Text("Connexion"),
+        backgroundColor: const Color.fromARGB(255, 162, 28, 64),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Image(image: AssetImage('images/produits/noel1.jpeg'), width: 300.0, height: 300.0),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Adresse email',
+                        labelStyle: TextStyle(color: Color.fromARGB(255, 162, 28, 64)),
                       ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'Mot de passe',
-                          labelStyle: TextStyle(color: Colors.white),
-                        ),
-                        obscureText: true,
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Veuillez entrer un mot de passe';
-                          }
-                          return null;
-                        },
-                        onSaved: (value) => _password = value!,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Veuillez entrer une adresse email valide';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) => _email = value!,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Mot de passe',
+                        labelStyle: TextStyle(color: Color.fromARGB(255, 162, 28, 64)),
                       ),
-                      const Padding(padding: EdgeInsets.all(8.0)),
-                      RaisedButton(
-                        child: const Text('Se connecter'),
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            _formKey.currentState!.save();
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => HomePage()),
-                            );
-                            // Appeler l'API de connexion ici avec _email et _password
-                          }
-                        },
-                      ),
-                    ],
-                  ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Veuillez entrer un mot de passe';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) => _password = value!,
+                    ),
+                    const Padding(padding: EdgeInsets.all(8.0)),
+                    RaisedButton(
+                      color: const Color.fromARGB(255, 162, 28, 64),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => HomePage()),
+                          );
+                          // Appeler l'API de connexion ici avec _email et _password
+                        }
+                      },
+                      child: const Text('Se connecter', style: TextStyle(color: Colors.white)),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
